369c8bba8560fb88935b2d8aa5319aeed00cb877
Speedberg <mattspeedberg@gmail.com>
2022-09-30 20:47:51 +0100

    Update Navbar links and logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import style from '../styles/component/Navbar.module.scss'
+
+import logo from '../public/logo/logo_full.svg';
 
 export default function Navbar(props) {
     return (
+        //TODO - navbar with icon in center above copyright
         <nav className={style.navbar}>
-            <h1><Link href='/'><a>McJAM</a></Link></h1>
+            <h1><Link href='/'><a><Image src={logo}/></a></Link></h1>
             <ul>
-                <li><Link href='/about'><a>About Us</a></Link></li>
-                <li><Link href='/contacts'><a>Contacts</a></Link></li>
+                <li><Link href='/games'><a>Games</a></Link></li>
+                <li><Link href='/about'><a>About</a></Link></li>
             </ul>
         </nav>
     )
