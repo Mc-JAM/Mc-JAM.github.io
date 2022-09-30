76045212eb0823b1065713c0884d7e06c03467d9
Speedberg <mattspeedberg@gmail.com>
2022-09-30 20:43:47 +0100

    Update About page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,17 +1,50 @@
-import styles from "../styles/pages/About.module.scss"
-import Abouts from "../components/Abouts"
 import Page from "../components/Page";
+import Content from "../components/Content";
+
+import contentStyles from "../styles/component/Content.module.scss"
+import style from "../styles/pages/About.module.scss";
+import Avatar from "../components/Avatar";
+
+import jjAvatar from '../public/img/avatars/jj.png';
+import jayAvatar from '../public/img/avatars/jay.jpeg';
+import aaronAvatar from '../public/img/avatars/aaron.jpg';
+import matthewAvatar from '../public/img/avatars/matthew.png';
 
 export default function About() {
     return (
         <Page page="about" title="About">
-            <h1>What We Do</h1>
-            <p>We are a small group of 4 who share a burning passion in games. We fuel our work with this passion we harbour to make games we can enjoy.</p>
-            <h1>Our Roles</h1>
-            <Abouts i="/img/justjjsimplified.png" a="JJ" desc="JJ, Programmer" />
-            <Abouts i="/img/jay.jpeg" a="Jay" desc="Jay, Modeller/2d artist" />
-            <Abouts i="/img/aaronsongaming.jpg" a="Aaron" desc="Aaron, Junior Programmer" />
-            <Abouts i="/img/speedberg.png" a="Matthew" desc="Matthew, Programmer/2d artist" />
+            <header>
+                <div>
+                    <h1>About Us</h1>
+                    <p>Our Story</p>
+                </div>
+            </header>
+            <Content>
+                <div className={`${contentStyles.section} ${contentStyles.small}`}>
+                    <h2>About Us</h2>
+                    <p>McJAM is a small group of game developers who share a burning passion in making games. Founded in 2022, we are realtively new to the scene - however, our desire to learn and create amazing content fuels us as we aim to make something which everyone can enjoy.</p>
+                </div>
+
+                <h2>Team Members</h2>
+                <div className={style.members}>
+                    <Avatar name="JJ" title="Programmer" source={jjAvatar} links={{
+                        itchdotio: "https://thediamonds559.itch.io",
+                        github:"https://github.com/TheDiamonds559"
+                    }}/>
+                    <Avatar name="Jay" title="3D Modeller" source={jayAvatar} links={{
+                        itchdotio: "https://jayy518.itch.io/",
+                        github:"https://github.com/Jayy518"
+                    }}/>
+                    <Avatar name="Aaron" title="Programmer" source={aaronAvatar} links={{
+                        itchdotio: "https://aaronsongaming.itch.io/",
+                        github:"https://github.com/AaronsonGaming"
+                    }}/>
+                    <Avatar name="Matthew" title="Programmer, 2D Artist" source={matthewAvatar} links={{
+                        itchdotio: "https://speedberg.itch.io/",
+                        github:"https://github.com/Speedberg"
+                    }}/>
+                </div>
+            </Content>
         </Page>
     )
 }
